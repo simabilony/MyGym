89d1a19eb714204c6af87e0a705bebfda9e40e41
simabilony <simabilony@gmail.com>
2024-04-30 18:10:56 +0300

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'
     ];
 
     /**
@@ -40,6 +40,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+
     ];
 
     public function scheduledClasses() {
